da03ef8eb4aeb1aded16c312a2e7d9ea4c2fdefc
Manferno <manuel.rcb.mc@gmail.com>
2023-06-11 00:03:15 -0400

    Todo para arriba señores

---
+++ src/main/java/com/example/test_email/TestEmailApplication.java
@@ -0,0 +1,15 @@
+package com.example.test_email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.regex.Pattern;
+
+@SpringBootApplication
+public class TestEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestEmailApplication.class, args);
+	}
+
+}
+
